7a23db3125c241b32322e82f1a5bf79b15542c65
shayanbzoral <55618250+ghayas110@users.noreply.github.com>
2024-01-22 16:06:54 +0500

    SignUp Flow

---
+++ components/ButtonInput.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const ButtonInput = ({onPress}) => {
+  return (
+    <View>
+    <TouchableOpacity onPress={onPress}>
+    <Text>ButtonInput</Text>
+    </TouchableOpacity>
+    </View>
+  )
+}
+
+export default ButtonInput
+
+const styles = StyleSheet.create({})
