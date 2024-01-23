cc790f3d82365c36ed8513364ebd3abc821c0285
0318119 <professionalalexwilson@gmail.com>
2024-01-22 22:42:44 -0800

    hamza

---
+++ screens/TopUp/TopUpFaq.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, Image, Animated, Easing } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+const TopUp = () => {
+  return (
+    <>
+        <Text style={{fontSize:16, color:"black" , fontFamily:"Poppins"}}>Taha</Text>
+    </>
+  )
+}
+
+export default TopUp
+
+const styles = StyleSheet.create({
+  
+})
