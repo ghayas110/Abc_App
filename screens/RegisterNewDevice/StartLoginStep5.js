b50d699c143fdf36ff587e29d05780525fbb21e3
Mustufa <mustufakhanofficial97@gmail.com>
2024-02-02 19:27:38 +0500

    qachanges

--- screens/RegisterNewDevice/StartLoginStep5.js
+++ screens/RegisterNewDevice/StartLoginStep5.js
@@ -3,25 +3,23 @@ import React, { useState, useRef } from 'react';
 import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity, Alert, Dimensions, KeyboardAvoidingView } from 'react-native';
 import ButtonInput from '../../components/ButtonInput';
 import { useDispatch } from 'react-redux';
+import { RequestButton } from '../../components/Buttons';
 import { loginSuccess } from '../../redux/reducers/authReducer';
-import { useNavigation } from '@react-navigation/native'
 import style from '../../assets/styles/basic';
+import { useNavigation } from '@react-navigation/native';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const StartLoginStep5 = ({ onPress,route }) => {
   const [otp, setOtp] = useState(['', '', '', '']);
   const dispatch=useDispatch()
-const navigation =useNavigation()
+  const { width, height } = Dimensions.get('window');
+  const navigation =useNavigation()
+
   const [email, setEmail] = useState(route?.params?.bodys?.email);
   const [user_type, setuser_type] = useState(route?.params?.bodys?.user_type)
   const inputRefs = useRef(Array(4).fill(0).map((_, i) => i));
   console.log(route?.params?.bodys?.user_type,"sss")
-  
-  const ForWordnavigation = () => {
-    navigation.navigate('StartLoginStep6');
-};
-
   const handleInputChange = (index, value) => {
     const newOtp = [...otp];
     newOtp[index] = value;
@@ -72,16 +70,28 @@ const navigation =useNavigation()
   };
 
   return (
-    <KeyboardAvoidingView 
+    <KeyboardAvoidingView
     behavior={Platform.OS === "ios" ? "padding" : "height"} 
-    style={styles.container}
+    style={{
+      ...style.basic_container,
+      height: height*0.95,
+      position: "relative",
+      backgroundColor: "white"
+  }}
   >
-    <View style={styles.container}>
-    <View>
+        
+              {/* INPUT BOX ============================= */}
+              <View style={{
+                  ...style.basic_container,
+                  marginTop: 20,
+                  paddingLeft: 30,
+                  paddingRight: 30
+              }}>
+          <View>
     <Text 
-    style={{ ...style.main_headings, textAlign: "left", fontSize: 25, fontFamily: "Poppins-Bold" }}
+    style={{ ...style.main_heading, textAlign: "left", fontSize: 25, fontFamily: "Poppins-Bold" ,textTransform:'none',marginTop:15 }}
     >Enter your One-Time Password (OTP)</Text>
-    <Text style={styles.text2}>We have sent your one time password to ‘+96XXXXX1234’</Text>
+    <Text style={styles.text3}>We have sent your one time password to ‘+96XXXXX1234’</Text>
 
       <View style={styles.inputContainer}>
         {otp.map((digit, index) => (
@@ -89,9 +99,9 @@ const navigation =useNavigation()
             key={index}
             ref={(ref) => (inputRefs.current[index] = ref)}
             style={styles.input}
-            keyboardType='numeric'
             maxLength={1}
             value={digit}
+            keyboardType='numeric'
             placeholderTextColor='white'
             onChangeText={(value) => handleInputChange(index, value)}
             onKeyPress={({ nativeEvent }) => {
@@ -103,17 +113,13 @@ const navigation =useNavigation()
         ))}
       </View>
       </View>
-      <View>
-      <Button
-title="Continue"
-color="green"
-style={{borderRadius:50,borderWidth:1}}
-onPress={()=>ForWordnavigation()}
-/>
-</View>
-      
-    </View>
-    </KeyboardAvoidingView>
+              </View>
+              <RequestButton text={"Next"} onPress={()=>navigation.navigate('StartLoginStep6')}
+
+                  btnStyle={{ position: "absolute", bottom: 50}}
+              />
+     
+          </KeyboardAvoidingView>
   );
 };
 
@@ -121,12 +127,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding:20,
-    height:windowHeight*0.9,
+    height:windowHeight*0.95,
     
    justifyContent:'space-between'
   },
   text2:{
     fontSize: 17,        
+    color: "#808080",
+    fontFamily: "Poppins-Regular"
+  },
+  text3:{
+    fontSize: 15,        
     color: "#808080",
     fontFamily: "Poppins-Regular"
   },
@@ -153,7 +164,7 @@ const styles = StyleSheet.create({
     height: 40,
     width: 40,
     borderBottomWidth: 1, // This adds a bottom border
-    borderBottomColor: 'green', // This sets the bottom border color to black
+    borderBottomColor: 'grey', // This sets the bottom border color to black
     margin: 5,
     textAlign: 'center',
     color: 'green',
@@ -172,5 +183,8 @@ const styles = StyleSheet.create({
 });
 
 export default StartLoginStep5;
+
+
+
 
 
